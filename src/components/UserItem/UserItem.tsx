cddac9dcd9f2897755b4a07cd4e08fbd663f9d22
kiianskii <ankiyanskiy@gmail.com>
2024-08-30 14:43:26 +0300

    Added table with users

---
+++ src/components/UserItem/UserItem.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import s from "./UserItem.module.css";
+
+interface User {
+  user: {
+    name: string;
+    username: string;
+    email: string;
+    phone: string;
+  };
+}
+
+const UserItem: React.FC<User> = ({ user }) => {
+  return (
+    <tr className={s.rowline}>
+      <td className={s.row}>{user.name}</td>
+      <td className={s.row}>{user.username}</td>
+      <td className={s.row}> {user.email}</td>
+      <td className={s.row}>{user.phone}</td>
+    </tr>
+  );
+};
+
+export default UserItem;
--- src/components/UsersTable/UsersTable.tsx
+++ src/components/UsersTable/UsersTable.tsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectUsers } from "../../redux/users/slice";
+import UserItem from "../UserItem/UserItem";
+import s from "./UserTable.module.css";
 
 const UsersTable = () => {
   const users = useSelector(selectUsers);
 
   return (
-    <ul>
-      {users.map((user) => {
-        return (
-          <li>
-            <h3>Name: {user.name}</h3>
-            <h3>Username: {user.username}</h3>
-            <h3>Email: {user.email}</h3>
-            <h3>Phone: {user.phone}</h3>
-          </li>
-        );
-      })}
-    </ul>
+    <div>
+      <table className={s.table}>
+        <thead className={s.thead}>
+          <tr className={s.tr}>
+            <th className={s.headline}>Name</th>
+            <th className={s.headline}>Username</th>
+            <th className={s.headline}>Email</th>
+            <th className={s.headline}>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => {
+            return <UserItem user={user} />;
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
