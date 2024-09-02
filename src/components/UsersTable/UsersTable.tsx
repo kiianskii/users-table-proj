2e3a7de98f921e6b6d9ff8f8e124c4d026f4031d
kiianskii <ankiyanskiy@gmail.com>
2024-09-02 10:34:33 +0300

    fixed table

--- src/components/UsersTable/UsersTable.tsx
+++ src/components/UsersTable/UsersTable.tsx
@@ -9,25 +9,29 @@ import { selectFilteredUsers } from "../../redux/selectors";
 const UsersTable = () => {
   const users = useSelector(selectFilteredUsers);
 
-  return (
-    <div>
-      <table className={s.table}>
-        <thead className={s.thead}>
-          <tr className={s.tr}>
-            <th className={s.headline}>Name</th>
-            <th className={s.headline}>Username</th>
-            <th className={s.headline}>Email</th>
-            <th className={s.headline}>Phone</th>
-          </tr>
-        </thead>
-        <tbody className={s.body}>
-          {users.map((user) => {
-            return <UserItem user={user} />;
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+  if (users.length === 0) {
+    return <h2>No users avaliable</h2>;
+  } else {
+    return (
+      <div>
+        <table className={s.table}>
+          <thead className={s.thead}>
+            <tr className={s.tr}>
+              <th className={s.headline}>Name</th>
+              <th className={s.headline}>Username</th>
+              <th className={s.headline}>Email</th>
+              <th className={s.headline}>Phone</th>
+            </tr>
+          </thead>
+          <tbody className={s.body}>
+            {users.map((user) => {
+              return <UserItem key={user.username} user={user} />;
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 };
 
 export default UsersTable;
